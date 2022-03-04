5b292ae5b59e5b231feb6d7900eff4337f02dd85
tattommasi <tatiana.tommasi@polito.it>
2022-03-04 15:58:03 +0100

    upload

---
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
@@ -0,0 +1,54 @@
+package it.polito.tdp.parole;
+
+import it.polito.tdp.parole.model.Parole;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class FXMLController {
+	
+	Parole elenco ;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtParola;
+
+    @FXML
+    private Button btnInserisci;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    void doInsert(ActionEvent event) {
+    	// TODO
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+    	// TODO
+    }
+
+    @FXML
+    void initialize() {
+        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
+
+        elenco = new Parole() ;
+    }
+}
---
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
@@ -0,0 +1,24 @@
+package it.polito.tdp.parole.model;
+
+import java.util.List;
+
+public class Parole {
+		
+	public Parole() {
+		//TODO
+	}
+	
+	public void addParola(String p) {
+		//TODO
+	}
+	
+	public List<String> getElenco() {
+		//TODO
+		return null;
+	}
+	
+	public void reset() {
+		// TODO
+	}
+
+}
